e735607d2e8f7ff693a202c96e1d70fd1c7fb67c
lmw <alsdndjdrktl@gmail.com>
2022-12-10 17:03:44 +0900

    feat(#19): firebase 로그인 연동
    - firebase 로그인 연동 구현

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '~/components/molecules/header'
+import { auth } from '~/firebase'
 
 function Layout() {
+ const [userData, setUserData] = useState<User>()
+
+ function handleGoogleLogin() {
+  const provider = new GoogleAuthProvider()
+  signInWithPopup(auth, provider)
+   .then((data) => {
+    setUserData(data.user)
+   })
+   .catch((err) => {
+    console.log(err)
+   })
+ }
  return (
   <div className="w-full h-screen">
    <Header />
 
    <section>
+    <button onClick={handleGoogleLogin}>Login</button>
+    {userData ? userData.displayName : null}
     <Outlet />
    </section>
   </div>
